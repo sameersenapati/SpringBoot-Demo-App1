6e774b118cc2553bf1d4bfa9f31272f910cf0951
sameer <sameersenapatibr1997@gmail.com>
2020-06-09 12:30:16 +0530

    main class modified

--- src/main/java/com/sam/SpringBootDemoApp1Application.java
+++ src/main/java/com/sam/SpringBootDemoApp1Application.java
@@ -2,12 +2,14 @@ package com.sam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDemoApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApp1Application.class, args);
+		ConfigurableApplicationContext run=SpringApplication.run(SpringBootDemoApp1Application.class, args);
+		run.close();
 	}
 
 }
