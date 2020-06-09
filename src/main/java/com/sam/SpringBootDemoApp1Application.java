46e701c899ff08fc694774ec8fc9a1216ba622e2
sameer <sameersenapatibr1997@gmail.com>
2020-06-09 12:06:56 +0530

    Project initial commit

---
+++ src/main/java/com/sam/SpringBootDemoApp1Application.java
@@ -0,0 +1,13 @@
+package com.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApp1Application.class, args);
+	}
+
+}
